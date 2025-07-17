299f21fb1693ace9bb19098489d1d41e0e37c893
Saverio Monaco <samonaco@max-display009.desy.de>
2025-07-17 10:30:58 +0200

    updated gen notebook

--- src/mua/unitaries/Uuat.py
+++ src/mua/unitaries/Uuat.py
@@ -5,14 +5,15 @@ from ..utils import dev1
 
 
 def ansatz(p_x, p_phi, pp_w, p_alpha, wire=0):
-    def U(p_x, p_phi, p_w, p_alpha, wire):
+    def U(p_x, phi, p_w, alpha, wire):
         qml.RZ(2 * (jnp.dot(p_w, p_x) + alpha), wires=wire)
         qml.RY(2 * phi, wires=wire)
 
     for phi, p_w, alpha in zip(p_phi, pp_w, p_alpha):
         U(p_x, phi, p_w, alpha, wire)
 
-@qml.qnode(dev1, interface='jax')
+
+@qml.qnode(dev1, interface="jax")
 def Uuat(x, p_phi, pp_w, p_alpha):
     ansatz(x, p_phi, pp_w, p_alpha)
     return qml.expval(qml.PauliZ(0))
